55aa326211634956c956c5c33dc32a0e3ee58e5b
vatg6791-byte <vatg6791@gmail.com>
2026-09-10 09:38:03 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    API_KEY: "AQ.Ab8RN6Liqo30Uo9QdmfpQdL8aW_MTbjJGlsJ3c_lTh2xY6TbKw",
+    API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent"
+};
